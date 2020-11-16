ef8512188da7657c0befc2868faf9780e1b1cf97
yeomsteve <jjojjosteve@gmail.com>
2020-11-16 11:49:01 -0500

    2 files modified

--- src/app/myresume/myresume.component.ts
+++ src/app/myresume/myresume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { r1 } from '../products';
 
 @Component({
   selector: 'app-myresume',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./myresume.component.css']
 })
 export class MyresumeComponent implements OnInit {
-
+ profile = r1.profile;
+ workExpeience = r1.workExperience ;
+ education = r1.education ;
   constructor() { }
 
   ngOnInit() {
--- src/app/products.ts
+++ src/app/products.ts
@@ -15,6 +15,77 @@ export const products = [
     description: ''
   }
 ];
+export const r1 = {
+  "profile":{
+  "firstName": "Jane",
+  "lastName": "Smith",
+  "isAlive": true,
+  "age": 27,
+  "photo": "https://www.w3schools.com/w3images/avatar_hat.jpg",
+  "profession": "Designer",
+  "address": {
+    "streetAddress": "21 2nd Street",
+    "city": "New York ",
+    "state": "NY",
+    "postalCode": "10021-3100",
+    "country": "USA",
+    "email": "ex@mail.com"
+  },
+  "phoneNumbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+"languages":[
+	{
+		"language": "English",
+		"level": "100%"
+	},
+	
+		{
+		"language": "Spanish",
+		"level": "50%"
+	},
+		{
+		"language": "German",
+		"level": "25%"
+	}
+	
+	],
+
+"skills":[
+	{
+		"skill": "Adobe Photoshop",
+		"level": "95%"
+	},
+	
+		{
+		"skill": "Photography",
+		"level": "80%"
+	},
+		{
+		"skill": "Illustrator",
+		"level": "85%"
+	},
+	
+		{
+		"skill": "Media",
+		"level": "50%"
+	}
+	],
+
+  },
+  
+  
+  "workExperience": [],
+  "education": []
+}
+
 
 
 /*
